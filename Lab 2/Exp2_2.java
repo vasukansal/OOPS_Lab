e1426ec5f4aa1b7b338b8a239c4d96fd6bef9ecc
Vasu Kansal <vasu12b8075@gmail.com>
2022-09-23 14:38:03 +0530

    Experiment - 2

---
+++ Lab 2/Exp2_2.java
@@ -0,0 +1,39 @@
+//Write a program to accept 10 student’s marks in an array, arrange it into ascending order, convertinto the grades and print marks and grades in the tabular form.
+
+import java.util.Scanner;
+
+public class Exp2_2 {
+    // #4
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        System.out.println("Enter the marks -> ");
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(arr);
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] < 40) {
+                System.out.println(arr[i] + " - Fail");
+            } else if (40 <= arr[i] && arr[i] <= 50) {
+                System.out.println(arr[i] + " - Only Pass");
+            } else if (51 >= arr[i] && arr[i] <= 75) {
+                System.out.println(arr[i] + " - Merit");
+            } else {
+                System.out.println(arr[i] + " - Excellent");
+            }
+        }
+
+    }
+
+    public static void sort(int[] arr) {
+        int temp;
+        for (int i = 1; i < 10; i++) {
+            if (arr[i] > arr[i - 1]) {
+                temp = arr[i - 1];
+                arr[i - 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
