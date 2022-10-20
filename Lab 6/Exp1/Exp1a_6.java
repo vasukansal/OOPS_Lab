f8a2f65bc43eb888c048e549225912d56c9028d7
Vasu Kansal <vasu12b8075@gmail.com>
2022-10-20 20:31:37 +0530

    Lab Experiments

---
+++ Lab 6/Exp1/Exp1a_6.java
@@ -0,0 +1,13 @@
+//Write a Java program to implement the concept of importing classes from user defined package andcreated packages.
+
+package Exp1;
+
+import Exp1.Exp1b_6;
+import java.util.Scanner;
+
+public class Exp1a_6 {
+    public static void main(String[] args) {
+        Exp1b_6 obj = new Exp1b_6();
+        obj.sum();
+    }
+}
---
+++ Lab 6/Exp1/Exp1b_6.java
@@ -0,0 +1,10 @@
+package Exp1;
+
+public class Exp1b_6 {
+    int A = 20;
+    int B = 30;
+
+    void sum() {
+        System.out.println(this.A + this.B);
+    }
+}
