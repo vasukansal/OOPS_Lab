4f21b8da0c60bf4fb9b8025ed8babb9288d5c073
Vasu Kansal <vasu12b8075@gmail.com>
2022-11-15 22:38:27 +0530

    Lab Experiments

--- Lab 7/Exp3_7.java
+++ Lab 7/Exp3_7.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Exp3_7 {
-    class exception{
-        void modules(int a,int b){
-            
+    public static void modules() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the Operands");
+        try {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a % b);
+            sc.close();
+        } catch (Exception e) {
+            System.out.println("Enter valid numeric  operand");
         }
     }
+
     public static void main(String[] args) {
-        
+        modules();
     }
 }
